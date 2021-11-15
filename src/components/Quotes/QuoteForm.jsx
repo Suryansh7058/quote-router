b6d3862252733d7109311b7649e9bba73ede0f94
Suryansh7058 <83452887+Suryansh7058@users.noreply.github.com>
2021-11-16 00:28:32 +0530

    Used useHistory,useParams and useLocation to Handle Fallback for url not found in QuoteDetail.jsx,used Prompt Component from react-router-dom to Handle Accidental returns from unsubmitted forms once focussed in QuoteForms.jsx

--- src/components/Quotes/QuoteForm.jsx
+++ src/components/Quotes/QuoteForm.jsx
@@ -1,46 +1,63 @@
-import { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Form, { Control, Actions, Loading } from './QuoteForm.styled';
 import { Card } from '../UI/Card.styled';
 import { Button } from '../UI/Buttons.styled';
 import { LoadingSpinner } from '../UI/LoadingSpinner.styled';
+import { Prompt } from 'react-router-dom';
 
 const QuoteForm = (props) => {
+  const [formFocus, setFormFocus] = useState(false);
+
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
   function submitFormHandler(event) {
     event.preventDefault();
-
     const enteredAuthor = authorInputRef.current.value;
     const enteredText = textInputRef.current.value;
-
-    // optional: Could validate here
-
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <Form onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <Loading>
-            <LoadingSpinner />
-          </Loading>
-        )}
+  const onFocusChangeHandler = () => {
+    setFormFocus(true);
+  };
 
-        <Control>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </Control>
-        <Control>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" rows="5" ref={textInputRef}></textarea>
-        </Control>
-        <Actions>
-          <Button>Add Quote</Button>
-        </Actions>
-      </Form>
-    </Card>
+  const finishEnteringHandler = () => {
+    setFormFocus(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Prompt
+        when={formFocus}
+        message={(location) =>
+          'Are you sure you want to leave? All your entered data will be lost'
+        }
+      />
+      <Card>
+        <Form onFocus={onFocusChangeHandler} onSubmit={submitFormHandler}>
+          {props.isLoading && (
+            <Loading>
+              <LoadingSpinner />
+            </Loading>
+          )}
+
+          <Control>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </Control>
+          <Control>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </Control>
+          <Actions>
+            <Button onClick={finishEnteringHandler} type="submit">
+              Add Quote
+            </Button>
+          </Actions>
+        </Form>
+      </Card>
+    </React.Fragment>
   );
 };
 
--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { useParams, Route } from 'react-router';
+import { useParams, Route, useHistory } from 'react-router';
 import Comments from '../components/Comments/Comments';
 import HighlightedQuote from '../components/Quotes/HighlightedQuote';
+import { StyledLinkFlat } from '../NavLink';
+import { useRouteMatch } from 'react-router';
 const DUMMY_QUOTES = [
   { id: 'q1', author: 'Suryansh', text: 'Learning React is fun!' },
   { id: 'q2', author: 'Suryansh Singh', text: 'Learning React is great!' },
@@ -9,20 +10,27 @@ const DUMMY_QUOTES = [
 
 const QuoteDetail = () => {
   const params = useParams();
-
+  const history = useHistory();
+  const match = useRouteMatch();
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-    return <p>No quote Found</p>;
+    history.replace('/*');
   }
-
   return (
-    <div>
+    <>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route exact path={match.path}>
+        <div className="centered">
+          <StyledLinkFlat to={`${match.url}/comments`}>
+            Load Comments
+          </StyledLinkFlat>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
-    </div>
+    </>
   );
 };
 
