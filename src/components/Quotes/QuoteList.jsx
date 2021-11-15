10b10f89921b4aa4c7acf049f880682bf9a2b9a2
Suryansh7058 <83452887+Suryansh7058@users.noreply.github.com>
2021-11-16 00:32:23 +0530

    Added Sorting function to sort the list in  ascending and descending order modified route paths using functional history push and useRouteMatch hhook

--- src/components/Quotes/QuoteList.jsx
+++ src/components/Quotes/QuoteList.jsx
@@ -1,12 +1,44 @@
 import { Fragment } from 'react';
 import QuoteItem from './QuoteItem';
-import QuoteListUl from './QuoteList.styled';
+import QuoteListUl, { Sorting } from './QuoteList.styled';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+  const match = useRouteMatch();
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    history.push({
+      pathname: match.path,
+      search: `?sort=${isSortingAscending ? 'desc' : 'asc'}`,
+    });
+    // history.push(`${match.url}?sort=${isSortingAscending ? 'desc' : 'asc'}`);
+  };
   return (
     <Fragment>
+      <Sorting>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </Sorting>
       <QuoteListUl>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import QuoteForm from '../components/Quotes/QuoteForm';
 const NewQuote = (props) => {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
